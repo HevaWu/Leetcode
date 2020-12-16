0ef6485233b7f4389d3a21fe0686158a032a7906
He Wu(Heva) <hewu9375@gmail.com>
2020-12-16 21:54:32 +0900

    Add Validate_Binary_Search_Tree.swift

---
+++ Validate_Binary_Search_Tree.swift
@@ -0,0 +1,62 @@
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+*/
+
+/*
+Solution 1: DFS
+
+recursively check if subTree is BST by 
+setting minVal and maxVal
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        return isValidBST(root, Int.min, Int.max)
+    }
+    
+    func isValidBST(_ node: TreeNode?, _ minVal: Int, _ maxVal: Int) -> Bool {
+        guard let node = node else { return true }
+        if node.val >= maxVal || node.val <= minVal { return false }
+        return isValidBST(node.left, minVal, node.val) 
+        && isValidBST(node.right, node.val, maxVal)
+    }
+}
