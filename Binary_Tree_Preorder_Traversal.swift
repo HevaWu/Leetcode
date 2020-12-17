190c210d778b67b7ec41aaea0fa8b62f28ef56d4
He Wu(Heva) <hewu9375@gmail.com>
2020-12-17 22:04:50 +0900

    Add Binary_Tree_Preorder_Traversal.swift

---
+++ Binary_Tree_Preorder_Traversal.swift
@@ -0,0 +1,74 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+*/
+
+/*
+Solution 1:
+recursively traverse binary tree
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var res = [Int]()
+        _preorder(root, &res)
+        return res
+    }
+    
+    func _preorder(_ node: TreeNode?, _ res: inout [Int]) {
+        guard let node = node else { return }
+        res.append(node.val)
+        _preorder(node.left, &res)
+        _preorder(node.right, &res)
+    }
+}
