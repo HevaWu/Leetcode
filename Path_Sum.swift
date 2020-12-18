f4918da0579bf843b84654e343ba4191e5df133a
He Wu(Heva) <hewu9375@gmail.com>
2020-12-18 22:40:23 +0900

    Add Path_Sum.swift

---
+++ Path_Sum.swift
@@ -0,0 +1,81 @@
+/*
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+*/
+
+/*
+Solution 1: 
+recursive
+
+return true if 
+node.left == nil, node.right == nil
+cur+node.val == sum
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        return _pathSum(root, 0, sum)
+    }
+    
+    func _pathSum(_ node: TreeNode?, _ cur: Int, _ sum: Int) -> Bool {
+        guard let node = node else { return false }
+        if node.left == nil, node.right == nil { 
+            return (cur+node.val) == sum
+        }
+        return _pathSum(node.left, cur+node.val, sum) 
+        || _pathSum(node.right, cur+node.val, sum)
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        guard let root = root else { return false }
+        if root.left == nil, root.right == nil, root.val == sum {
+            return true
+        }
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val)
+    }
+}
