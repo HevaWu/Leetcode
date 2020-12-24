2ab971bac0170550019c4b762e84b9583b55908f
He Wu(Heva) <hewu9375@gmail.com>
2020-12-24 23:51:20 +0900

    Add Swap_Nodes_in_Pairs.swift

---
+++ Swap_Nodes_in_Pairs.swift
@@ -0,0 +1,58 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes. Only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+*/
+
+/*
+Solution 1:
+recursively
+
+swap first 2 nodes, then append swapPairs(head.next.next) to it
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        guard let _head = head, 
+        let headNext = _head.next else { 
+            return head 
+        }
+        
+        // later recursive node
+        let temp = headNext.next
+        headNext.next = _head
+        _head.next = swapPairs(temp)
+        return headNext
+    }
+}
