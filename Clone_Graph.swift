8cf3bc4f4c34aa2d2afdede24ad2404dbcb8ac48
He Wu(Heva) <hewu9375@gmail.com>
2021-01-02 19:11:28 +0900

    Add Clone_Graph.swift

---
+++ Clone_Graph.swift
@@ -0,0 +1,102 @@
+/*
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity sake, each node's value is the same as the node's index (1-indexed). For example, the first node with val = 1, the second node with val = 2, and so on. The graph is represented in the test case using an adjacency list.
+
+Adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+Example 2:
+
+
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+Example 3:
+
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
+Example 4:
+
+
+Input: adjList = [[2],[1]]
+Output: [[2],[1]]
+ 
+
+Constraints:
+
+1 <= Node.val <= 100
+Node.val is unique for each node.
+Number of Nodes will not exceed 100.
+There is no repeated edges and no self-loops in the graph.
+The Graph is connected and all nodes can be visited starting from the given node.
+*/
+
+/*
+Solution 1: 
+recursively copy graph node
+
+use visited to help memo visited node
+- key: node.val
+- value: newNode
+
+
+*/
+
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var neighbors: [Node?]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.neighbors = []
+ *     }
+ * }
+ */
+
+class Solution {
+    var visited: [Int: Node?] = [:]
+    
+    func cloneGraph(_ node: Node?) -> Node? {
+        guard let node = node else { return nil }
+        
+        if let newNode = visited[node.val] {
+            return newNode
+        }
+        
+        var newNode = Node(node.val)
+        visited[node.val] = newNode
+		// newNode.neighbors = node.neighbors.map { cloneGraph($0) }
+        for n in node.neighbors {
+            newNode.neighbors.append(cloneGraph(n))
+        }
+        return newNode
+    }
+}
