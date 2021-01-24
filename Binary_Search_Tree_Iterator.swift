2f2d98638c62c7e8f54d5d0adc7d364181c3cfa3
He Wu(Heva) <hewu9375@gmail.com>
2021-01-24 19:21:23 +0900

    Add Binary_Search_Tree_Iterator.swift

---
+++ Binary_Search_Tree_Iterator.swift
@@ -0,0 +1,163 @@
+/*
+mplement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+Example 1:
+
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+0 <= Node.val <= 106
+At most 105 calls will be made to hasNext, and next.
+ 
+
+Follow up:
+
+Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+*/
+
+/*
+Solution 2:
+use stack to keep saving BST left
+
+Time Complexity:
+- next: O(1)
+- hasNext: O(1)
+
+Space Complexity: O(h)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+class BSTIterator {
+
+    var stack = [TreeNode]()
+    
+    init(_ root: TreeNode?) {
+        var cur = root
+        while cur != nil {
+            stack.append(cur!)
+            cur = cur?.left
+        }
+    }
+    
+    func next() -> Int {
+        let cur = stack.removeLast()
+        if cur.right != nil {
+            var temp = cur.right
+            while temp != nil {
+                stack.append(temp!)
+                temp = temp?.left
+            }
+        }
+        return cur.val
+    }
+    
+    func hasNext() -> Bool {
+        return !stack.isEmpty
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * let obj = BSTIterator(root)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
+
+/*
+Solution 1:
+use array to store inorder traversal result of tree
+
+Time Complexity: O(1)
+Space Complexity: O(n)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class BSTIterator {
+
+    var arr: [Int] = [Int]()
+    var cur = 0
+    
+    init(_ root: TreeNode?) {
+        inorder(root)
+    }
+    
+    func inorder(_ root: TreeNode?) {
+        guard let node = root else { return }
+        inorder(node.left)
+        arr.append(node.val)
+        inorder(node.right)
+    }
+    
+    /** @return the next smallest number */
+    func next() -> Int {
+        cur += 1
+        return arr[cur-1]
+    }
+    
+    /** @return whether we have a next smallest number */
+    func hasNext() -> Bool {
+        return cur < arr.count
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * let obj = BSTIterator(root)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
