b0102b522c2ed2ccd7ab37db457bd1ef847af1cd
He Wu(Heva) <hewu9375@gmail.com>
2021-03-10 20:32:15 +0900

    Add Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.swift

---
+++ Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.swift
@@ -0,0 +1,92 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+Example 2:
+
+Input: prices = [1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= prices.length <= 5000
+0 <= prices[i] <= 1000
+*/
+
+/*
+Solution 1:
+DP
+
+constant space
+
+idea:
+The natural states for this problem is the 3 possible transactions : buy, sell, rest. Here rest means no transaction on that day (aka cooldown).
+
+Then the transaction sequences can end with any of these three states.
+
+For each of them we make an array, buy[n], sell[n] and rest[n].
+
+buy[i] means before day i what is the maxProfit for any sequence end with buy.
+
+sell[i] means before day i what is the maxProfit for any sequence end with sell.
+
+rest[i] means before day i what is the maxProfit for any sequence end with rest.
+
+Then we want to deduce the transition functions for buy sell and rest. By definition we have:
+
+buy[i]  = max(rest[i-1]-price, buy[i-1]) 
+sell[i] = max(buy[i-1]+price, sell[i-1])
+rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+Where price is the price of day i. All of these are very straightforward. They simply represents :
+
+(1) We have to `rest` before we `buy` and 
+(2) we have to `buy` before we `sell`
+One tricky point is how do you make sure you sell before you buy, since from the equations it seems that [buy, rest, buy] is entirely possible.
+
+Well, the answer lies within the fact that buy[i] <= rest[i] which means rest[i] = max(sell[i-1], rest[i-1]). That made sure [buy, rest, buy] is never occurred.
+
+A further observation is that and rest[i] <= sell[i] is also true therefore
+
+rest[i] = sell[i-1]
+Substitute this in to buy[i] we now have 2 functions instead of 3:
+
+buy[i] = max(sell[i-2]-price, buy[i-1])
+sell[i] = max(buy[i-1]+price, sell[i-1])
+This is better than 3, but
+
+we can do even better
+
+Since states of day i relies only on i-1 and i-2 we can reduce the O(n) space to O(1).
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {        
+        var sell = 0
+        var pre_sell = 0
+        var buy = Int.min
+        var pre_buy = buy
+        
+        for price in prices {
+            pre_buy = buy
+            buy = max(pre_sell - price, pre_buy)
+            pre_sell = sell
+            sell = max(pre_buy + price, pre_sell)
+        }
+        
+        return sell
+    }
+}
