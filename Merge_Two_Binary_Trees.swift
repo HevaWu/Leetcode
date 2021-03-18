e9c165d70b2ff7f65fdaf7a7424f9bf3c8a3f834
He Wu(Heva) <hewu9375@gmail.com>
2021-03-19 08:55:30 +0900

    Add Merge_Two_Binary_Trees.swift

---
+++ Merge_Two_Binary_Trees.swift
@@ -0,0 +1,61 @@
+/*
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 2000].
+-104 <= Node.val <= 104
+*/
+
+/*
+Solution 1:
+recursively merge 2 root
+
+Time Complexity: O(n), n is maximum node count of root1 and root2
+Space Complexity: O(n)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        if root1 == nil, root2 == nil { return nil }
+        if root1 == nil { return root2 }
+        if root2 == nil { return root1 }
+        let mergedNode = TreeNode(root1!.val + root2!.val)
+        mergedNode.left = mergeTrees(root1?.left, root2?.left)
+        mergedNode.right = mergeTrees(root1?.right, root2?.right)
+        return mergedNode
+    }
+}
