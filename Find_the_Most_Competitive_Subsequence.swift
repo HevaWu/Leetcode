74a3ef25d3e87a37e957d621d36eb579eee2963f
He Wu(Heva) <hewu9375@gmail.com>
2021-04-05 20:03:23 +0900

    Add some comment

--- Find_the_Most_Competitive_Subsequence.swift
+++ Find_the_Most_Competitive_Subsequence.swift
@@ -5,7 +5,7 @@ An array's subsequence is a resulting sequence obtained by erasing some (possibl
 
 We define that a subsequence a is more competitive than a subsequence b (of the same length) if in the first position where a and b differ, subsequence a has a number less than the corresponding number in b. For example, [1,3,4] is more competitive than [1,3,5] because the first position they differ is at the final number, and 4 is less than 5.
 
- 
+
 
 Example 1:
 
@@ -16,7 +16,7 @@ Example 2:
 
 Input: nums = [2,4,3,3,5,4,9,6], k = 4
 Output: [2,3,3,4]
- 
+
 
 Constraints:
 
@@ -32,28 +32,30 @@ In lexicographical order, the elements to the left have higher priority than tho
 Solution 1:
 use stack to handle current mapped element
 
-- if k+i-stack.count < n, valid element, prepare append it
+- if i+(k-stack.count) < n, valid element, prepare append it
+=> k-stack.count, number still need to be find
+=> i+requiredFindNumber < n, make sure have enough space to find remaining number
 
 Time Complexity: O(n)
 Space Complexity: O(k)
 */
 class Solution {
-    func mostCompetitive(_ nums: [Int], _ k: Int) -> [Int] {        
+    func mostCompetitive(_ nums: [Int], _ k: Int) -> [Int] {
         // index stack
         var stack = [Int]()
-        
+
         let n = nums.count
         for (i,v) in nums.enumerated() {
-            while !stack.isEmpty, 
-            v < stack.last!, 
-            k+i-stack.count < n {
+            while !stack.isEmpty,
+            v < stack.last!,
+            i+k-stack.count < n {
                 stack.removeLast()
             }
             if stack.count < k {
                 stack.append(v)
             }
         }
-        
+
         return stack
     }
 }
