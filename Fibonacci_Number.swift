a75f4b77c58b65f6eb202c94d46219a095478d17
He Wu(Heva) <hewu9375@gmail.com>
2021-04-15 19:19:01 +0900

    Fix format

--- Fibonacci_Number.swift
+++ Fibonacci_Number.swift
@@ -5,7 +5,7 @@ F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
 Given n, calculate F(n).
 
- 
+
 
 Example 1:
 
@@ -22,7 +22,7 @@ Example 3:
 Input: n = 4
 Output: 3
 Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
- 
+
 
 Constraints:
 
@@ -61,24 +61,24 @@ class Solution {
         matrixPower(&A, n-1)
         return A[0][0]
     }
-    
+
     func matrixPower(_ A: inout [[Int]], _ n: Int) {
         if n <= 1 { return }
         matrixPower(&A, n/2)
         multiply(&A, A)
-        
+
         var B = [[1, 1], [1, 0]]
         if n%2 != 0 {
             multiply(&A, B)
         }
     }
-    
+
     func multiply(_ A: inout [[Int]], _ B: [[Int]]) {
         let x = A[0][0] * B[0][0] + A[0][1] * B[1][0]
         let y = A[0][0] * B[0][1] + A[0][1] * B[1][1]
         let z = A[1][0] * B[0][0] + A[1][1] * B[1][0]
         let w = A[1][0] * B[0][1] + A[1][1] * B[1][1]
-        
+
         A[0][0] = x
         A[0][1] = y
         A[1][0] = z
@@ -100,17 +100,17 @@ class Solution {
         // key is number n
         // value is F(n): Fibonacci value
         var cache = [Int: Int]()
-        
+
         cache[0] = 0
         cache[1] = 1
-        
+
         func _fib(_ num: Int) -> Int {
             if let val = cache[num] { return val }
             cache[num-1] = _fib(num-1)
             cache[num-2] = _fib(num-2)
             return cache[num-1]! + cache[num-2]!
         }
-        
+
         return _fib(n)
     }
 }
