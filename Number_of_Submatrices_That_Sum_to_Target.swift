a8da321a8caa836737bf1fb16b8d086d72712c6d
He Wu(Heva) <hewu9375@gmail.com>
2021-04-17 21:21:57 +0900

    Fix format

--- Number_of_Submatrices_That_Sum_to_Target.swift
+++ Number_of_Submatrices_That_Sum_to_Target.swift
@@ -4,7 +4,7 @@
 
 // Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
 
- 
+
 
 // Example 1:
 
@@ -16,7 +16,7 @@
 // Input: matrix = [[1,-1],[-1,1]], target = 0
 // Output: 5
 // Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
- 
+
 
 // Note:
 
@@ -25,26 +25,26 @@
 // -1000 <= matrix[i] <= 1000
 // -10^8 <= target <= 10^8
 
-// Solution 1: 
+// Solution 1:
 // For each row, calculate the prefix sum.
 // For each pair of columns,
 // calculate the accumulated sum of rows.
 // Now this problem is same to, "Find the Subarray with Target Sum".
-// 
+//
 // Time O(N^3)
 // Space O(N)
 class Solution {
     func numSubmatrixSumTarget(_ matrix: [[Int]], _ target: Int) -> Int {
         let n = matrix.count
         let m = matrix[0].count
-        
+
         var matrix = matrix
         for i in 0..<n {
             for j in 1..<m {
                 matrix[i][j] = matrix[i][j-1] + matrix[i][j]
             }
         }
-        
+
         var count = 0
         for y1 in 0..<m {
             for y2 in y1..<m {
