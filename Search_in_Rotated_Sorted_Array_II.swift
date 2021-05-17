4fe48f7887dc8687d99fd90d0b3d2441d6f4d84a
He Wu(Heva) <hewu9375@gmail.com>
2021-05-17 10:39:48 +0900

    Add Search_in_Rotated_Sorted_Array_II.swift

---
+++ Search_in_Rotated_Sorted_Array_II.swift
@@ -0,0 +1,76 @@
+/*
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+
+
+Follow up: This problem is the same as Search in Rotated Sorted Array, where nums may contain duplicates. Would this affect the runtime complexity? How and why?
+*/
+
+/*
+Solution 1:
+binary search
+
+shorten left, right if left+1, right-1 is duplicated
+then do same thing as Search in rotated sorted array
+
+Time Complexity: worst O(n) best O(logn)
+Space Complexity: O(1)
+*/
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        let n = nums.count
+        var left = 0
+        var right = n-1
+
+        while left <= right {
+            while left < right, nums[left] == nums[left+1] {
+                left += 1
+            }
+
+            while left < right, nums[right] == nums[right-1] {
+                right -= 1
+            }
+
+            let mid = left + (right-left)/2
+            if nums[mid] == target { return true }
+
+            if nums[left] <= nums[mid] {
+                if nums[left] <= target && target < nums[mid] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else {
+                if nums[mid] < target && target <= nums[right] {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+
+        return false
+    }
+}
