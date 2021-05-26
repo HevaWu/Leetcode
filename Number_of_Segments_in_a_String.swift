393e6a13567d990e40ba471d2a0991ad37c9a3a2
He Wu(Heva) <hewu9375@gmail.com>
2021-05-26 11:23:01 +0900

    Add Number_of_Segments_in_a_String.swift

---
+++ Number_of_Segments_in_a_String.swift
@@ -0,0 +1,45 @@
+/*
+You are given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+Example 3:
+
+Input: s = "love live! mu'sic forever"
+Output: 4
+Example 4:
+
+Input: s = ""
+Output: 0
+
+
+Constraints:
+
+0 <= s.length <= 300
+s consists of lower-case and upper-case English letters, digits or one of the following characters "!@#$%^&*()_+-=',.:".
+The only space character in s is ' '.
+*/
+
+/*
+Solution 1:
+String split function
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func countSegments(_ s: String) -> Int {
+        return s.split(separator: " ").count
+    }
+}
