149c573d5d651c6c2f4f7bb8f96e653631ddf8a1
He Wu(Heva) <hewu9375@gmail.com>
2021-06-05 20:54:59 +0900

    Add Longest_ZigZag_Path_in_a_Binary_Tree.swift

---
+++ Longest_ZigZag_Path_in_a_Binary_Tree.swift
@@ -0,0 +1,91 @@
+/*
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+Choose any node in the binary tree and a direction (right or left).
+If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
+Change the direction from right to left or from left to right.
+Repeat the second and third steps until you can't move in the tree.
+Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1]
+Output: 3
+Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
+Example 2:
+
+
+Input: root = [1,1,1,null,1,null,null,1,1,null,1]
+Output: 4
+Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
+Example 3:
+
+Input: root = [1]
+Output: 0
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5 * 104].
+1 <= Node.val <= 100
+
+*/
+
+/*
+Solution 1:
+recursive
+
+Time Complexity: O(n)
+n is total number of node in the tree
+
+Space Complexity: O(1)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func longestZigZag(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+
+        var longest = 0
+        check(root.left, true, 0, &longest)
+        check(root.right, false, 0, &longest)
+        return longest
+    }
+
+    func check(_ node: TreeNode?, _ isLeft: Bool,
+               _ curLen: Int, _ longest: inout Int) {
+        guard let node = node else {
+            longest = max(longest, curLen)
+            return
+        }
+
+        // clear to 0 if 2 same direction connected
+        if isLeft {
+            check(node.left, true, 0, &longest)
+            check(node.right, false, curLen+1, &longest)
+        } else {
+            check(node.left, true, curLen+1, &longest)
+            check(node.right, false, 0, &longest)
+        }
+    }
+}
