c7fe44a9c55d024eb9746b26059b54b522022224
He Wu(Heva) <hewu9375@gmail.com>
2021-06-29 09:37:30 +0900

    Add Remove_All_Adjacent_Duplicates_In_String.swift

---
+++ Remove_All_Adjacent_Duplicates_In_String.swift
@@ -0,0 +1,50 @@
+/*
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation:
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+*/
+
+/*
+Solution 1:
+use stack
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    func removeDuplicates(_ s: String) -> String {
+        var stack = [Character]()
+
+        for c in s {
+            if !stack.isEmpty, stack.last! == c {
+                while !stack.isEmpty, stack.last! == c {
+                    stack.removeLast()
+                }
+            } else {
+                stack.append(c)
+            }
+        }
+        return String(stack)
+    }
+}
