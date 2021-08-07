4003f6cf2d05ce00a79f651ab29353784b97ca88
He Wu(Heva) <hewu9375@gmail.com>
2021-08-07 15:18:29 +0900

    Add Max_Increase_to_Keep_City_Skyline.swift

---
+++ Max_Increase_to_Keep_City_Skyline.swift
@@ -0,0 +1,71 @@
+/*
+There is a city composed of n x n blocks, where each block contains a single building shaped like a vertical square prism. You are given a 0-indexed n x n integer matrix grid where grid[r][c] represents the height of the building located in the block at row r and column c.
+
+A city's skyline is the the outer contour formed by all the building when viewing the side of the city from a distance. The skyline from each cardinal direction north, east, south, and west may be different.
+
+We are allowed to increase the height of any number of buildings by any amount (the amount can be different per building). The height of a 0-height building can also be increased. However, increasing the height of a building should not affect the city's skyline from any cardinal direction.
+
+Return the maximum total sum that the height of the buildings can be increased by without changing the city's skyline from any cardinal direction.
+
+
+
+Example 1:
+
+
+Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+Output: 35
+Explanation: The building heights are shown in the center of the above image.
+The skylines when viewed from each cardinal direction are drawn in red.
+The grid after increasing the height of buildings without affecting skylines is:
+gridNew = [ [8, 4, 8, 7],
+            [7, 4, 7, 7],
+            [9, 4, 8, 7],
+            [3, 3, 3, 3] ]
+Example 2:
+
+Input: grid = [[0,0,0],[0,0,0],[0,0,0]]
+Output: 0
+Explanation: Increasing the height of any building will result in the skyline changing.
+
+
+Constraints:
+
+n == grid.length
+n == grid[r].length
+2 <= n <= 50
+0 <= grid[r][c] <= 100
+
+*/
+
+/*
+Solution 1:
+
+1. get left and up skyline of grid
+2. find minimum height we can assign for i,j, and we add increase by (minHeight-grid[i][j])
+
+Time Complexity: O(n*n)
+Space Complexity: O(n)
+*/
+class Solution {
+    func maxIncreaseKeepingSkyline(_ grid: [[Int]]) -> Int {
+        let n = grid.count
+
+        var left = Array(repeating: 0, count: n)
+        var up = Array(repeating: 0, count: n)
+
+        for i in 0..<n {
+            for j in 0..<n {
+                left[i] = max(left[i], grid[i][j])
+                up[j] = max(up[j], grid[i][j])
+            }
+        }
+
+        var increase = 0
+        for i in 0..<n {
+            for j in 0..<n {
+                increase += (min(left[i], up[j]) - grid[i][j])
+            }
+        }
+        return increase
+    }
+}
