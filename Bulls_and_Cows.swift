302ec0495a110ab68b8e7658b895f45282db8331
He Wu(Heva) <hewu9375@gmail.com>
2021-08-15 13:26:54 +0900

    update code

--- Bulls_and_Cows.swift
+++ Bulls_and_Cows.swift
@@ -1,6 +1,6 @@
 // You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
 
-// Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
+// Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows.
 
 // Please note that both secret number and friend's guess may contain duplicate digits.
 
@@ -21,38 +21,44 @@
 // Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
 
 // Solution 1: 2 array
-// one for secret 
+// one for secret
 // one for guess
 // use these two to help checking cows
-// 
+//
 // Time complexity: O(n)
 // Space complexity: O(1)
 class Solution {
     func getHint(_ secret: String, _ guess: String) -> String {
-        var secretArr = Array(repeating: 0, count: 10)
-        var guessArr = Array(repeating: 0, count: 10)
-        
-        var bulls = 0
-        var cows = 0
+        let n = secret.count
+
+        var sArr = Array(repeating: 0, count: 10)
+        var gArr = Array(repeating: 0, count: 10)
+
+        // bull
+        var x = 0
+
+        // cow
+        var y = 0
+
         for i in secret.indices {
             if secret[i] == guess[i] {
-                bulls += 1
+                x += 1
             } else {
-                secretArr[Int(String(secret[i]))!] += 1
-                guessArr[Int(String(guess[i]))!] += 1
+                sArr[secret[i].wholeNumberValue!] += 1
+                gArr[guess[i].wholeNumberValue!] += 1
             }
         }
-        
-        for i in 0..<10 {
-            cows += min(secretArr[i], guessArr[i])
+
+        for d in 0...9 {
+            y += max(0, min(sArr[d], gArr[d]))
         }
-        
-        return "\(bulls)A\(cows)B"
+
+        return "\(x)A\(y)B"
     }
 }
 
 // Solution 2: optimize one array
-// 
+//
 // Time complexity: O(n)
 // Space complexity: O(1)
 class Solution {
