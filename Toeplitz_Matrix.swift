68f19b9ebff5be1b7325dc05c9b259393c5cb438
He Wu(Heva) <hewu9375@gmail.com>
2021-08-18 21:14:11 +0900

    Add Toeplitz_Matrix.swift

---
+++ Toeplitz_Matrix.swift
@@ -0,0 +1,78 @@
+/*
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99
+
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+
+*/
+
+/*
+Solution 1:
+iterate matrix by frist column, first row
+try to check if element in diagonal start from this cell is equal to each other or not
+
+Time Complexity: O(mn)
+Space Complexity: O(1)
+*/
+class Solution {
+    func isToeplitzMatrix(_ matrix: [[Int]]) -> Bool {
+        let m = matrix.count
+        let n = matrix[0].count
+
+        if m == 1 || n == 1 { return true }
+
+        for i in stride(from: m-2, through: 0, by: -1) {
+            let val = matrix[i][0]
+
+            // i+len<m
+            // len < n
+            for len in 1..<min(n, m-i) {
+                if matrix[i+len][len] != val { return false }
+            }
+        }
+
+        for j in 1..<n {
+            let val = matrix[0][j]
+
+            // len < m
+            // j+len < n
+            for len in 1..<min(m, n-j) {
+                if matrix[len][j+len] != val { return false }
+            }
+        }
+
+        return true
+    }
+}
