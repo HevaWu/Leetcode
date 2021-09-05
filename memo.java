453a5983e686fcab112e1ce334e67f2ae7a4e6eb
He Wu(Heva) <hewu9375@gmail.com>
2021-09-05 18:43:42 +0900

    add memo.java

---
+++ memo.java
@@ -0,0 +1,7 @@
+// ======= String =======
+char[] arr = s.toCharArray(); //string to char array
+s.substring(i) + s.substring(0, i); // substring
+s1.compareTo(s2); // compare 2 strings, 0 means equal, <0 means s1 < s2, >0 means s1 > s2
+
+// ======= Arrays =======
+Arrays.sort(arr) // sort array
