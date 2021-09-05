f1efd54f976235ea2bf369eba12a7a815c7e1822
He Wu(Heva) <hewu9375@gmail.com>
2021-09-05 18:44:13 +0900

    Add Unique_Binary_Search_Trees_II.java

---
+++ Unique_Binary_Search_Trees_II.java
@@ -0,0 +1,71 @@
+/*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+
+
+Constraints:
+
+0 <= n <= 8
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return buildTree(1, n);
+    }
+
+    public List<TreeNode> buildTree(int start, int end) {
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftList = buildTree(start, i-1);
+            List<TreeNode> rightList = buildTree(i+1, end);
+            for (TreeNode l : leftList) {
+                for (TreeNode r : rightList) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = l;
+                    node.right = r;
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
+}
