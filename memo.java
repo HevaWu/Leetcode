51f3800bb584366cd92b17ff56dc39f618dafef0
He Wu(Heva) <hewu9375@gmail.com>
2021-09-06 18:17:56 +0900

    Add Slowest_Key

--- memo.java
+++ memo.java
@@ -2,6 +2,7 @@
 char[] arr = s.toCharArray(); //string to char array
 s.substring(i) + s.substring(0, i); // substring
 s1.compareTo(s2); // compare 2 strings, 0 means equal, <0 means s1 < s2, >0 means s1 > s2
+s.charAt(0); // return char at index
 
 // ======= Arrays =======
 Arrays.sort(arr); // sort array
