1d9eeb60d58d47cb0630bb39c0bc96732bc422ff
He Wu(Heva) <hewu9375@gmail.com>
2021-09-30 12:00:18 +0900

    Add Split_Linked_List_in_Parts.java

---
+++ Split_Linked_List_in_Parts.java
@@ -0,0 +1,83 @@
+/*
+Given a (singly) linked list with head node root, write a function to split the linked list into k consecutive linked list "parts".
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than 1. This may lead to some parts being null.
+
+The parts should be in order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal parts occurring later.
+
+Return a List of ListNode's representing the linked list parts that are formed.
+
+Examples 1->2->3->4, k = 5 // 5 equal parts [ [1], [2], [3], [4], null ]
+Example 1:
+Input:
+root = [1, 2, 3], k = 5
+Output: [[1],[2],[3],[],[]]
+Explanation:
+The input and each element of the output are ListNodes, not arrays.
+For example, the input root has root.val = 1, root.next.val = 2, \root.next.next.val = 3, and root.next.next.next = null.
+The first element output[0] has output[0].val = 1, output[0].next = null.
+The last element output[4] is null, but it's string representation as a ListNode is [].
+Example 2:
+Input:
+root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 3
+Output: [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+Explanation:
+The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+Note:
+
+The length of root will be in the range [0, 1000].
+Each value of a node in the input will be an integer in the range [0, 999].
+k will be an integer in the range [1, 50].
+*/
+
+/*
+Solution 1:
+using for loop
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int n = 0;
+        ListNode node = head;
+        while (node != null) {
+            node = node.next;
+            n += 1;
+        }
+
+        node = head;
+        int size = n / k;
+        int remain = n % k;
+        ListNode[] res = new ListNode[k];
+
+        for (int i = 0; i < k; i++) {
+            ListNode cur = node;
+            int temp = size + (i < remain ? 0 : -1);
+            for (int j = 0; j < temp; j++) {
+                if (node != null) {
+                    node = node.next;
+                }
+            }
+
+            if (node != null) {
+                ListNode pre = node;
+                node = node.next;
+                pre.next = null;
+            }
+
+            res[i] = cur;
+        }
+        return res;
+    }
+}
