5e8d69520825902bb3f7f94d5f3571ca3242270a
He Wu(Heva) <hewu9375@gmail.com>
2021-10-02 21:14:17 +0900

    Add Search_Insert_Position.java

--- Search_Insert_Position.java
+++ Search_Insert_Position.java
@@ -20,13 +20,24 @@ Hide Similar Problems (E) First Bad Version
 */
 
 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
 
-
-/////////////////////////////////////////////////////////////////////////////////////
-//C++
-
-
-
+        while (left < right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] < target ? left+1 : left;
+    }
+}
 
 
 /*binary search
---
+++ Search_Insert_Position.swift
@@ -0,0 +1,56 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+Example 4:
+
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+Example 5:
+
+Input: nums = [1], target = 0
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+*/
+
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count-1
+
+        while left < right {
+            let mid = left + (right - left)/2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] < target {
+                left = mid+1
+            } else {
+                right = mid
+            }
+        }
+        return nums[left] < target ? left+1 : left
+    }
+}
