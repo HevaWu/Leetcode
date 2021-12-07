e4309e72f668b359b4decf37ce09e683907e1c6a
He Wu(Heva) <hewu9375@gmail.com>
2021-12-07 20:36:22 +0900

    Add Convert_Binary_Number_in_a_Linked_List_to_Integer

---
+++ Convert_Binary_Number_in_a_Linked_List_to_Integer.swift
@@ -0,0 +1,92 @@
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+
+
+Example 1:
+
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+Example 2:
+
+Input: head = [0]
+Output: 0
+Example 3:
+
+Input: head = [1]
+Output: 1
+Example 4:
+
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+Example 5:
+
+Input: head = [0,0]
+Output: 0
+
+
+Constraints:
+
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+*/
+
+/*
+Solution 2:
+bit manipulation
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func getDecimalValue(_ head: ListNode?) -> Int {
+        var val = 0
+        var cur = head
+        while cur != nil {
+            val = (val << 1) | cur!.val
+            cur = cur!.next
+        }
+        return val
+    }
+}
+
+/*
+Solution 1:
+update val by each node's value
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func getDecimalValue(_ head: ListNode?) -> Int {
+        var val = 0
+        var cur = head
+        while cur != nil {
+            val = val * 2 + cur!.val
+            cur = cur!.next
+        }
+        return val
+    }
+}
