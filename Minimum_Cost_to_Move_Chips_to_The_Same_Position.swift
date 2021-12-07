9d34ba0081983143af82a5bf2e5599d1a1c892bb
He Wu(Heva) <hewu9375@gmail.com>
2021-12-07 20:36:36 +0900

    Add Minimum_Cost_to_Move_Chips_to_The_Same_Position

---
+++ Minimum_Cost_to_Move_Chips_to_The_Same_Position.swift
@@ -0,0 +1,62 @@
+/*
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+
+
+Example 1:
+
+
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+Example 2:
+
+
+Input: position = [2,2,2,3,3]
+Output: 2
+Explanation: We can move the two chips at position  3 to position 2. Each move has cost = 1. The total cost = 2.
+Example 3:
+
+Input: position = [1,1000000000]
+Output: 1
+
+
+Constraints:
+
+1 <= position.length <= 100
+1 <= position[i] <= 10^9
+*/
+
+/*
+Solution 1:
+because change position +-2 cost 0
+move all even position coins together
+move all odd position coins Together
+
+min(even coins, odd coins)
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func minCostToMoveChips(_ position: [Int]) -> Int {
+        var even = 0
+        var odd = 0
+        for p in position {
+            if p % 2 == 0 {
+                even += 1
+            } else {
+                odd += 1
+            }
+        }
+        return min(even, odd)
+    }
+}
