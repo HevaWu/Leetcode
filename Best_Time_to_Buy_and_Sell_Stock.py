d4fea838e8f963c5ca35bda573fee2c612c7bb60
He(Heva) Wu <hewu9375@gmail.com>
2022-02-01 18:52:37 +0900

    update Best_Time_to_Buy_and_Sell_Stock

--- Best_Time_to_Buy_and_Sell_Stock.py
+++ Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,4 +1,5 @@
-/* Say you have an array for which the ith element is the price of a given stock on day i.
+'''
+ Say you have an array for which the ith element is the price of a given stock on day i.
 
  If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
@@ -15,29 +16,23 @@
  Input: [7,6,4,3,1]
  Output: 0
  Explanation: In this case, no transaction is done, i.e. max profit = 0.
-*/
+'''
 
-/*
+'''
  Solution 1: Greedy
  keep buy as the lowest price
  and keep finding the largest profit for each day
  
  Time complexity: O(n)
  Space complexity: O(1)
- */
-class Solution {
-    func maxProfit(_ prices: [Int]) -> Int {
-        guard !prices.isEmpty else { return 0 }
-        
-        var profit = 0
-        var buy = prices[0]
-        for i in 1..<prices.count {
-            if prices[i] < buy {
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
                 buy = prices[i]
-            } else {
+            else:
                 profit = max(profit, prices[i] - buy)
-            }
-        }
         return profit
-    }
-}
