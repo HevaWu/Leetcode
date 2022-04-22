3ac66799f47dc63662fb15b5d62c14bb652cd676
He Wu <hewu9375@gmail.com>
2022-04-22 21:36:59 +0900

    update Design_HashMap

---
+++ Design_HashMap.java
@@ -0,0 +1,62 @@
+/*
+Design a HashMap without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+
+Example:
+
+MyHashMap hashMap = new MyHashMap();
+hashMap.put(1, 1);
+hashMap.put(2, 2);
+hashMap.get(1);            // returns 1
+hashMap.get(3);            // returns -1 (not found)
+hashMap.put(2, 1);          // update the existing value
+hashMap.get(2);            // returns 1
+hashMap.remove(2);          // remove the mapping for 2
+hashMap.get(2);            // returns -1 (not found)
+
+Note:
+
+All keys and values will be in the range of [0, 1000000].
+The number of operations will be in the range of [1, 10000].
+Please do not use the built-in HashMap library.
+*/
+
+/*
+Solution 1:
+store element in array
+*/
+class MyHashMap {
+    int[] arr;
+
+    public MyHashMap() {
+        this.arr = new int[1_000_001];
+        for(int i = 0; i < 1_000_001; i++) {
+            this.arr[i] = -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        this.arr[key] = value;
+    }
+
+    public int get(int key) {
+        return this.arr[key];
+    }
+
+    public void remove(int key) {
+        this.arr[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
