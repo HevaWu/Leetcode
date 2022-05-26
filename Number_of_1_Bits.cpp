9708f59b6c1ee44e6bbff7022b03e7b74385c5db
He Wu <hewu9375@gmail.com>
2022-05-26 19:01:38 +0900

    update Number_of_1_Bits

--- Number_of_1_Bits.cpp
+++ Number_of_1_Bits.cpp
@@ -21,21 +21,3 @@ public:
         return nbits;
     }
 };
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////
-//Java
-public class Solution {
-    // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
-        int ret = 0;
-        while(n!=0){
-            n &= n-1;
-            ret++;
-        }
-        
-        return ret;
-    }
-}
