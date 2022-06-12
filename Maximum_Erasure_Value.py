66336b25963aa55a4c227a1f5a06182b2480f3ea
He Wu <hewu9375@gmail.com>
2022-06-12 19:47:46 +0900

    update Maximum_Erasure_Value

---
+++ Maximum_Erasure_Value.py
@@ -0,0 +1,56 @@
+'''
+You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+
+
+Example 1:
+
+Input: nums = [4,2,4,5,6]
+Output: 17
+Explanation: The optimal subarray here is [2,4,5,6].
+Example 2:
+
+Input: nums = [5,2,1,2,5,2,1,2,5]
+Output: 8
+Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+'''
+
+'''
+Solution 1:
+2 pointer
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+'''
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        cur = 0
+        score = 0
+
+        l = 0
+        r = 0
+        n = len(nums)
+
+        unique = set()
+
+        while r < n:
+            while nums[r] in unique and l < r:
+                unique.remove(nums[l])
+                cur -= nums[l]
+                l += 1
+            unique.add(nums[r])
+            cur += nums[r]
+            score = max(score, cur)
+            r += 1
+
+        return score
