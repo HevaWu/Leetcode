636e3e1e79f06d14dfac68fcc7ee817e32e57595
He Wu <hewu9375@gmail.com>
2022-06-25 19:01:01 +0900

    update Non-decreasing_Array

--- Non-decreasing_Array.java
+++ Non-decreasing_Array.java
@@ -33,29 +33,25 @@ Time Complexity: O(n)
 Space Complexity: O(n)
 */
 class Solution {
-    func checkPossibility(_ nums: [Int]) -> Bool {
-        let n = nums.count
-        if n <= 2 { return true }
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        int change = 0;
 
-        // element require to be changed
-        var count = 0
-        var nums = nums
-
-        for i in 1..<n where count <= 1 {
-            if nums[i-1] > nums[i] {
-                count += 1
-                if i-2<0 || nums[i-2] <= nums[i] {
-                    nums[i-1] = nums[i]
+        for(int i = 1; i < n; i++) {
+            if (nums[i-1] > nums[i]) {
+                change += 1;
+                if (i-2 >= 0 && nums[i-2] > nums[i]) {
+                    nums[i] = nums[i-1];
                 } else {
-                    nums[i] = nums[i-1]
+                    nums[i-1] = nums[i];
                 }
 
-                if count >= 2 {
-                    return false
+                if (change >= 2) {
+                    return false;
                 }
             }
         }
 
-        return true
+        return true;
     }
 }
--- Non-decreasing_Array.py
+++ Non-decreasing_Array.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
 
 We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
@@ -22,40 +22,27 @@ Constraints:
 n == nums.length
 1 <= n <= 104
 -105 <= nums[i] <= 105
-*/
+'''
 
-/*
+'''
 Solution 1:
 
 When you find nums[i-1] > nums[i] for some i, you will prefer to change nums[i-1]'s value, since a larger nums[i] will give you more risks that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i], then you have to change nums[i]'s value instead, or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
 
 Time Complexity: O(n)
 Space Complexity: O(n)
-*/
-class Solution {
-    func checkPossibility(_ nums: [Int]) -> Bool {
-        let n = nums.count
-        if n <= 2 { return true }
-
-        // element require to be changed
-        var count = 0
-        var nums = nums
-
-        for i in 1..<n where count <= 1 {
-            if nums[i-1] > nums[i] {
-                count += 1
-                if i-2<0 || nums[i-2] <= nums[i] {
-                    nums[i-1] = nums[i]
-                } else {
+'''
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        n = len(nums)
+        change = 0
+        for i in range(1, n):
+            if nums[i-1] > nums[i]:
+                change += 1
+                if change >= 2:
+                    return False
+                if i-2 >= 0 and nums[i-2] > nums[i]:
                     nums[i] = nums[i-1]
-                }
-
-                if count >= 2 {
-                    return false
-                }
-            }
-        }
-
-        return true
-    }
-}
+                else:
+                    nums[i-1] = nums[i]
+        return True
