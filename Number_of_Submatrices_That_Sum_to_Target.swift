c2754c16b9eb673aa5784c6d35504825de96007f
He Wu <hewu9375@gmail.com>
2022-07-18 18:07:30 +0900

    update comment

--- Number_of_Submatrices_That_Sum_to_Target.swift
+++ Number_of_Submatrices_That_Sum_to_Target.swift
@@ -1,38 +1,42 @@
-// Given a matrix, and a target, return the number of non-empty submatrices that sum to target.
+/*
+Given a matrix, and a target, return the number of non-empty submatrices that sum to target.
 
-// A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
+ A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
 
-// Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
-
-
-
-// Example 1:
-
-// Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
-// Output: 4
-// Explanation: The four 1x1 submatrices that only contain 0.
-// Example 2:
-
-// Input: matrix = [[1,-1],[-1,1]], target = 0
-// Output: 5
-// Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
+ Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
 
 
-// Note:
 
-// 1 <= matrix.length <= 300
-// 1 <= matrix[0].length <= 300
-// -1000 <= matrix[i] <= 1000
-// -10^8 <= target <= 10^8
+ Example 1:
 
-// Solution 1:
-// For each row, calculate the prefix sum.
-// For each pair of columns,
-// calculate the accumulated sum of rows.
-// Now this problem is same to, "Find the Subarray with Target Sum".
-//
-// Time O(N^3)
-// Space O(N)
+ Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+ Output: 4
+ Explanation: The four 1x1 submatrices that only contain 0.
+ Example 2:
+
+ Input: matrix = [[1,-1],[-1,1]], target = 0
+ Output: 5
+ Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
+
+
+ Note:
+
+ 1 <= matrix.length <= 300
+ 1 <= matrix[0].length <= 300
+ -1000 <= matrix[i] <= 1000
+ -10^8 <= target <= 10^8
+*/
+
+/*
+ Solution 1:
+ For each row, calculate the prefix sum.
+ For each pair of columns,
+ calculate the accumulated sum of rows.
+ Now this problem is same to, "Find the Subarray with Target Sum".
+
+ Time O(N^3)
+ Space O(N)
+ */
 class Solution {
     func numSubmatrixSumTarget(_ matrix: [[Int]], _ target: Int) -> Int {
         let n = matrix.count
@@ -49,6 +53,8 @@ class Solution {
         for y1 in 0..<m {
             for y2 in y1..<m {
                 // init map [0: 1] which means y1==y2
+                // key is submatrix sum
+                // val is the number of submatrix has this sum
                 var map: [Int: Int] = [0: 1]
                 var temp = 0
                 for x in 0..<n {
