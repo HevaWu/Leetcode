b10a5de1ecf255df292fd24e1db6380211a104b6
He Wu <hewu9375@gmail.com>
2022-07-25 20:53:57 +0900

    update comment

--- Find_First_and_Last_Position_of_Element_in_Sorted_Array.swift
+++ Find_First_and_Last_Position_of_Element_in_Sorted_Array.swift
@@ -1,22 +1,26 @@
-// Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+/*
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
-// Your algorithm's runtime complexity must be in the order of O(log n).
+Your algorithm's runtime complexity must be in the order of O(log n).
 
-// If the target is not found in the array, return [-1, -1].
+If the target is not found in the array, return [-1, -1].
 
-// Example 1:
+Example 1:
 
-// Input: nums = [5,7,7,8,8,10], target = 8
-// Output: [3,4]
-// Example 2:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
 
-// Input: nums = [5,7,7,8,8,10], target = 6
-// Output: [-1,-1]
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+*/
 
-// Solution 1: binary search 2 pointer
-// set left & right pointer, and use pivot to check if we find the target
+/*
+Solution 1: binary search 2 pointer
+set left & right pointer, and use pivot to check if we find the target
 //
-// Time complexity: O(logn)
+Time complexity: O(logn)
+*/
 class Solution {
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
         guard !nums.isEmpty, nums.contains(target) else { return [-1, -1] }
@@ -27,7 +31,7 @@ class Solution {
         while left <= right {
             pivot = (left+right)/2
             if nums[pivot] == target {
-                // try to find the range
+                try to find the range
                 var lp = pivot - 1
                 while lp>=left, nums[lp]==target {
                     lp -= 1
@@ -49,9 +53,11 @@ class Solution {
     }
 }
 
-// Time complexity : O(\log_{10}(n))Because binary search cuts the search space roughly in half on each iteration, there can be at most \lceil \log_{10}(n) iterations. Binary search is invoked twice, so the overall complexity is logarithmic.
-// Space complexity : O(1)O(1)
-// All work is done in place, so the overall memory usage is constant.
+/*
+Time complexity : O(\log_{10}(n))Because binary search cuts the search space roughly in half on each iteration, there can be at most \lceil \log_{10}(n) iterations. Binary search is invoked twice, so the overall complexity is logarithmic.
+Space complexity : O(1)O(1)
+All work is done in place, so the overall memory usage is constant.
+*/
 class Solution {
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
         var lp = check(nums, target, true)
