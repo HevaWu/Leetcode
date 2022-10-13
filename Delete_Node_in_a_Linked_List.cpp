f764093803f76978a8ef8ba03a3716b6e41c7c3a
He Wu <hewu9375@gmail.com>
2022-10-13 18:19:00 +0900

    update Delete_Node_in_a_Linked_List

--- Delete_Node_in_a_Linked_List.cpp
+++ Delete_Node_in_a_Linked_List.cpp
@@ -4,6 +4,13 @@ Write a function to delete a node (except the tail) in a singly linked list, giv
 Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
 */
 
+/*
+Solution 1
+Since we do not have access to the node before the one we want to delete, we cannot modify the next pointer of that node in any way. Instead, we have to replace the value of the node we want to delete with the value in the node after it, and then delete the node after it.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,9 +19,29 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    void deleteNode(ListNode* node) {
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
         ListNode *nextNode = node->next;
         *node = *nextNode;
         delete nextNode;
