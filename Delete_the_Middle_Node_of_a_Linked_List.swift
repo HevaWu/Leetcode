2ccea66a5e702a054ec84e4a037210c619d792bb
He(Heva) Wu <hewu9375@gmail.com>
2022-10-29 17:01:02 +0900

    add Delete_the_Middle_Node_of_a_Linked_List

---
+++ Delete_the_Middle_Node_of_a_Linked_List.swift
@@ -0,0 +1,75 @@
+/*
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+
+
+Example 1:
+
+
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node.
+Example 2:
+
+
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+Example 3:
+
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+1 <= Node.val <= 105
+*/
+
+/*
+Solution 1:
+use a jump node to always keep jump twice
+use pre node to track the "previous node of should be deleted one"
+
+Time Complexity: O(n/2)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteMiddle(_ head: ListNode?) -> ListNode? {
+        var dummy = ListNode(0)
+        dummy.next = head
+        var pre: ListNode? = dummy
+        var jump = head?.next
+        while jump != nil {
+            pre = pre?.next
+            jump = jump?.next?.next
+        }
+        // remove pre.next will be result
+        pre?.next = pre?.next?.next
+        return dummy.next
+    }
+}
