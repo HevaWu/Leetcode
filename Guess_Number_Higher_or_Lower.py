cdde0f1f1f3ae20670d8822e27895e99bc2b1bff
He Wu <hewu9375@gmail.com>
2022-11-16 18:49:05 +0900

    update Guess_Number_Higher_or_Lower

---
+++ Guess_Number_Higher_or_Lower.py
@@ -0,0 +1,65 @@
+'''
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns 3 possible results:
+
+-1: The number I picked is lower than your guess (i.e. pick < num).
+1: The number I picked is higher than your guess (i.e. pick > num).
+0: The number I picked is equal to your guess (i.e. pick == num).
+Return the number that I picked.
+
+
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+Example 4:
+
+Input: n = 2, pick = 2
+Output: 2
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+'''
+
+'''
+Solution 1:
+Binary Search
+
+Time Complexity: O(logn)
+Space Complexity: O(1)
+'''
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l = 1
+        r = n
+        while True:
+            mid = l + (r-l) // 2
+            guessResult = guess(mid)
+            if guessResult == 0: return mid
+            elif guessResult > 0: l = mid+1
+            else: r = mid-1
+        return -1
