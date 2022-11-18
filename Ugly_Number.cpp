991cf97bd81441068907998106187f6515b2f752
He Wu <hewu9375@gmail.com>
2022-11-18 22:19:11 +0900

    update Ugly_Number

--- Ugly_Number.cpp
+++ Ugly_Number.cpp
@@ -1,11 +1,47 @@
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3,
+and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are
+limited to 2, 3, and 5. Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+
+
+Constraints:
+
+-231 <= n <= 231 - 1
+*/
+
+/*
+Solution 1:
+keep divide n from 2 to 6
+if remain n == 1 return true
+
+Time Complexity: O(logn)
+Space Complexity: O(1)
+*/
 class Solution {
-public:
-    bool isUgly(int num) {
-        for(int i = 2; i < 6 && num; i++)
-        {
-            while(num%i == 0)
-                num /= i;
-        }
-        return num == 1;
+ public:
+  bool isUgly(int num) {
+    for (int i = 2; i < 6 && num; i++) {
+      while (num % i == 0) num /= i;
     }
+    return num == 1;
+  }
 };
---
+++ Ugly_Number.py
@@ -0,0 +1,44 @@
+'''
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+
+
+Constraints:
+
+-231 <= n <= 231 - 1
+'''
+
+'''
+Solution 1:
+keep divide n from 2 to 6
+if remain n == 1 return true
+
+Time Complexity: O(logn)
+Space Complexity: O(1)
+'''
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        for i in range(2, 7):
+            if n <= 1: break
+            while n%i == 0:
+                n /= i
+        return n==1
