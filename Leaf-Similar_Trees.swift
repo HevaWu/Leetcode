213fb6e1b56354d9ca8d1bc6df030d3c54499511
He(Heva) Wu <hewu9375@gmail.com>
2022-12-08 17:25:39 +0900

    add Leaf-Similar_Trees.swift

---
+++ Leaf-Similar_Trees.swift
@@ -0,0 +1,79 @@
+/*
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+
+
+Constraints:
+
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200].
+*/
+
+/*
+Solution 1:
+build 2 tree's leaf sequence
+then compare if they are equal
+
+Time Complexity: O(n)
+Space Complexity: O(logn)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        return getLeaf(from: root1) == getLeaf(from: root2)
+    }
+
+    func getLeaf(from root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var stack: [TreeNode] = [root]
+        var arr = [Int]()
+        while !stack.isEmpty {
+            let cur = stack.removeLast()
+            if cur.left == nil, cur.right == nil {
+                arr.append(cur.val)
+            } else {
+                if let l = cur.left {
+                    stack.append(l)
+                }
+                if let r = cur.right {
+                    stack.append(r)
+                }
+            }
+        }
+        return arr
+    }
+}
