f2650fe2ea311bb9be67bfd6db2e252f1877f950
He Wu <hewu9375@gmail.com>
2022-12-12 14:24:32 +0900

    update Climbing_Stairs

--- Climbing_Stairs.swift
+++ Climbing_Stairs.swift
@@ -3,7 +3,7 @@ You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
+
 
 Example 1:
 
@@ -20,12 +20,33 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
- 
+
 
 Constraints:
 
 1 <= n <= 45
 */
+
+/*
+Solution 2:
+bottom up dp
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n == 1 { return 1 }
+        var dp = Array(repeating: 0, count: n+1)
+        dp[1] = 1
+        dp[2] = 2
+        guard n > 2 else { return dp[n] }
+        for i in 3...n {
+            dp[i] = dp[i-1] + dp[i-2]
+        }
+        return dp[n]
+    }
+}
 
 /*
 Solution 1:
@@ -45,7 +66,7 @@ class Solution {
         var cache = [Int: Int]()
         cache[0] = 1
         cache[1] = 1
-        
+
         func _count(_ n: Int) -> Int {
             if let val = cache[n] { return val }
             cache[n] = _count(n-1) + _count(n-2)
