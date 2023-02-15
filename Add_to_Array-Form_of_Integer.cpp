0df9dade3ba51ca40a79b16af1055da7cc96ff4c
He Wu <hewu9375@gmail.com>
2023-02-15 20:40:37 +0900

    update Add_to_Array-Form_of_Integer

---
+++ Add_to_Array-Form_of_Integer.cpp
@@ -0,0 +1,77 @@
+/*
+The array-form of an integer num is an array representing its digits in left to
+right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form
+of the integer num + k.
+
+
+
+Example 1:
+
+Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+Example 2:
+
+Input: num = [2,7,4], k = 181
+Output: [4,5,5]
+Explanation: 274 + 181 = 455
+Example 3:
+
+Input: num = [2,1,5], k = 806
+Output: [1,0,2,1]
+Explanation: 215 + 806 = 1021
+Example 4:
+
+Input: num = [9,9,9,9,9,9,9,9,9,9], k = 1
+Output: [1,0,0,0,0,0,0,0,0,0,0]
+Explanation: 9999999999 + 1 = 10000000000
+
+
+Constraints:
+
+1 <= num.length <= 104
+0 <= num[i] <= 9
+num does not contain any leading zeros except for the zero itself.
+1 <= k <= 104
+*/
+
+/*
+Solution 2:
+optimize solution 1
+instead of change k everytime,
+directly assign k as cur init value
+
+Continuing the example of 123 + 912, we start with [1, 2, 3+912]. Then we
+perform the addition 3+912, leaving 915. The 5 stays as the digit, while we
+'carry' 910 into the next column which becomes 91.
+
+We repeat this process with [1, 2+91, 5]. We have 93, where 3 stays and 90 is
+carried over as 9. Again, we have [1+9, 3, 5] which transforms into [1, 0, 3,
+5].
+
+Time Complexity: O(max(n, log_10 k))
+Space Complexity: O(1)
+*/
+class Solution {
+ public:
+  vector<int> addToArrayForm(vector<int>& num, int k) {
+    int n = num.size();
+    int i = n - 1;
+    while (i >= 0) {
+      k += num[i];
+      num[i] = k % 10;
+      k /= 10;
+      i -= 1;
+    }
+
+    while (k > 0) {
+      num.insert(num.begin(), k % 10);
+      k /= 10;
+    }
+
+    return num;
+  }
+};
