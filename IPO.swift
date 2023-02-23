9ff23066de234c43f99f84bdccaf72934799eef4
He Wu <hewu9375@gmail.com>
2023-02-23 19:28:33 +0900

    add IPO

---
+++ IPO.swift
@@ -0,0 +1,105 @@
+/*
+Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
+
+You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+
+Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+
+Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+
+The answer is guaranteed to fit in a 32-bit signed integer.
+
+
+
+Example 1:
+
+Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+Output: 4
+Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+After finishing it you will obtain profit 1 and your capital becomes 1.
+With capital 1, you can either start the project indexed 1 or the project indexed 2.
+Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+Example 2:
+
+Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+Output: 6
+
+
+Constraints:
+
+1 <= k <= 105
+0 <= w <= 109
+n == profits.length
+n == capital.length
+1 <= n <= 105
+0 <= profits[i] <= 104
+0 <= capital[i] <= 109
+*/
+
+/*
+Solution 1:
+greedy
+
+1. combine profits and capital into a project array
+2. sort project by increasing capital
+3. when still have remain k steps, keep check
+  - push current can start project into available
+  - always pick the largest profit project
+
+Time Complexity: O(nlogn)
+Space Complexity: O(n)
+*/
+class Solution {
+    func findMaximizedCapital(_ k: Int, _ w: Int, _ profits: [Int], _ capital: [Int]) -> Int {
+        let n = profits.count
+        var project = Array(repeating: [0, 0], count: n)
+        for i in 0..<n {
+            project[i] = [profits[i], capital[i]]
+        }
+
+        project.sort(by: { p1, p2 -> Bool in
+            // sort by increasing capital
+            return p1[1] < p2[1]
+        })
+
+        var w = w
+        var k = k
+        var i = 0
+        var available = [Int]() // sort by increasing profit
+        while k > 0 {
+            while i < n, project[i][1] <= w {
+                // push into available
+                insert(project[i][0], &available)
+                i += 1
+            }
+
+            if available.isEmpty { break }
+
+            // always pick the largest profit project
+            w += available.removeLast()
+            k -= 1
+        }
+        return w
+    }
+
+    func insert(_ profit: Int, _ available: inout [Int]) {
+        if available.isEmpty {
+            available.append(profit)
+            return
+        }
+
+        var l = 0
+        var r = available.count-1
+        while l < r {
+            let mid = l + (r-l)/2
+            if available[mid] < profit {
+                l = mid+1
+            } else {
+                r = mid
+            }
+        }
+        let index = available[l] < profit ? l+1 : l
+        available.insert(profit, at: index)
+    }
+}
