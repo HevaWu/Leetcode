5f0070c3c4e507b67d3416e2de09b315facf8a70
He Wu <hewu9375@gmail.com>
2023-05-15 18:02:59 +0900

    update Swapping_Nodes_in_a_Linked_List

---
+++ Swapping_Nodes_in_a_Linked_List.cpp
@@ -0,0 +1,80 @@
+/*
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node
+from the beginning and the kth node from the end (the list is 1-indexed).
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+*/
+
+/*
+Solution 2:
+optimize solution 1
+
+only switch node.val would be enough, don't care link logic
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* swapNodes(ListNode* head, int k) {
+    ListNode* cur = head;
+    int len = 1;
+    while (len < k) {
+      cur = cur->next;
+      len += 1;
+    }
+    ListNode* first = cur;
+    cur = cur->next;
+    ListNode* second = head;
+    while (cur != nullptr) {
+      cur = cur->next;
+      second = second->next;
+    }
+
+    int temp = second->val;
+    second->val = first->val;
+    first->val = temp;
+    return head;
+  }
+};
