50b179fbc797bd1773b764adcc61f6f5bdb6eb3f
He Wu <hewu9375@gmail.com>
2023-06-19 19:49:04 +0900

    add Find_the_Highest_Altitude.swift

---
+++ Find_the_Highest_Altitude.swift
@@ -0,0 +1,44 @@
+/*
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+Example 2:
+
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+
+Constraints:
+
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+*/
+
+/*
+Solution 1:
+iterate gain, check current val, and compare it with highest one
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func largestAltitude(_ gain: [Int]) -> Int {
+        var num = 0
+        var highest = 0
+        for g in gain {
+            num += g
+            highest = max(highest, num)
+        }
+        return highest
+    }
+}
