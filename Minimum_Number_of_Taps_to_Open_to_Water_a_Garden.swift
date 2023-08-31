c1e056ea79696cfb1144b2caaada632fb1f7f1f0
He Wu <hewu9375@gmail.com>
2023-08-31 20:30:58 +0900

    add Minimum_Number_of_Taps_to_Open_to_Water_a_Garden.swift

---
+++ Minimum_Number_of_Taps_to_Open_to_Water_a_Garden.swift
@@ -0,0 +1,59 @@
+/*
+There is a one-dimensional garden on the x-axis. The garden starts at the point 0 and ends at the point n. (i.e The length of the garden is n).
+
+There are n + 1 taps located at points [0, 1, ..., n] in the garden.
+
+Given an integer n and an integer array ranges of length n + 1 where ranges[i] (0-indexed) means the i-th tap can water the area [i - ranges[i], i + ranges[i]] if it was open.
+
+Return the minimum number of taps that should be open to water the whole garden, If the garden cannot be watered return -1.
+
+
+
+Example 1:
+
+
+Input: n = 5, ranges = [3,4,1,1,0,0]
+Output: 1
+Explanation: The tap at point 0 can cover the interval [-3,3]
+The tap at point 1 can cover the interval [-3,5]
+The tap at point 2 can cover the interval [1,3]
+The tap at point 3 can cover the interval [2,4]
+The tap at point 4 can cover the interval [4,4]
+The tap at point 5 can cover the interval [5,5]
+Opening Only the second tap will water the whole garden [0,5]
+Example 2:
+
+Input: n = 3, ranges = [0,0,0,0]
+Output: -1
+Explanation: Even if you activate all the four taps you cannot water the whole garden.
+
+
+Constraints:
+
+1 <= n <= 104
+ranges.length == n + 1
+0 <= ranges[i] <= 100
+*/
+
+
+/*
+Solution 1:
+DP
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+*/
+class Solution {
+    func minTaps(_ n: Int, _ ranges: [Int]) -> Int {
+        var dp = Array(repeating: n+2, count: n+1)
+        dp[0] = 0
+        for i in 0...n {
+            var j = max(i-ranges[i]+1, 0)
+            while j <= min(i+ranges[i], n) {
+                dp[j] = min(dp[j], dp[max(0, i-ranges[i])] + 1)
+                j += 1
+            }
+        }
+        return dp[n] < n+2 ? dp[n] : -1
+    }
+}
