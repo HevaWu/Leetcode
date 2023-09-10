7857d9b1a2fd850e39534d57959c563dc7b9e839
He(Heva) Wu <hewu9375@gmail.com>
2023-09-10 17:57:28 +0900

    update Count_All_Valid_Pickup_and_Delivery_Options

---
+++ Count_All_Valid_Pickup_and_Delivery_Options.py
@@ -0,0 +1,48 @@
+'''
+Given n orders, each order consist in pickup and delivery services.
+
+Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i).
+
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+
+
+Example 1:
+
+Input: n = 1
+Output: 1
+Explanation: Unique order (P1, D1), Delivery 1 always is after of Pickup 1.
+Example 2:
+
+Input: n = 2
+Output: 6
+Explanation: All possible orders:
+(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1).
+This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery 2.
+Example 3:
+
+Input: n = 3
+Output: 90
+
+
+Constraints:
+
+1 <= n <= 500
+'''
+
+'''
+Solution 2:
+optimize solution 1 with use only one variable
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+'''
+class Solution:
+    def countOrders(self, n: int) -> int:
+        if n == 1: return 1
+        mod = 1000000007
+        cur = 1
+        for i in range(1, n):
+            preLen = 2*i+1
+            cur = (cur * ((preLen * (preLen+1)) // 2)) % mod
+        return cur
