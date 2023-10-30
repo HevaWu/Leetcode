483ca5361fdd7c673b380179d83d9cc93a388803
He Wu <hewu9375@gmail.com>
2023-10-30 19:43:43 +0900

    add Sort_Integers_by_The_Number_of_1_Bits

---
+++ Sort_Integers_by_The_Number_of_1_Bits.swift
@@ -0,0 +1,59 @@
+/*
+You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+Return the array after sorting it.
+
+
+
+Example 1:
+
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+Example 2:
+
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explantion: All integers have 1 bit in the binary representation, you should just sort them in ascending order.
+
+
+Constraints:
+
+1 <= arr.length <= 500
+0 <= arr[i] <= 104
+*/
+
+/*
+Solution 1:
+use another function to check number of bit 1 in num,
+then use it at sort compare function
+
+Time Complexity: O(nlogn)
+Space Complexity: O(1)
+*/
+class Solution {
+    func sortByBits(_ arr: [Int]) -> [Int] {
+        return arr.sorted(by: { num1, num2 -> Bool in
+            let n1 = numOfOne(num1)
+            let n2 = numOfOne(num2)
+            return n1 == n2 ? num1 < num2 : n1 < n2
+        })
+    }
+
+    // return number of 1 in num
+    func numOfOne(_ num: Int) -> Int {
+        var one = 0
+        var num = num
+        while num != 0 {
+            if num % 2 == 1 {
+                one += 1
+            }
+            num /= 2
+        }
+        return one
+    }
+}
