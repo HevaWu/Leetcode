8f2e4acf99fd005b9976a1d83af159bc5bfe463b
He Wu <hewu9375@gmail.com>
2023-12-12 22:06:06 +0900

    add Maximum_Product_of_Two_Elements_in_an_Array.swift

---
+++ Maximum_Product_of_Two_Elements_in_an_Array.swift
@@ -0,0 +1,48 @@
+/*
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+
+Example 1:
+
+Input: nums = [3,4,5,2]
+Output: 12
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+Example 2:
+
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+Example 3:
+
+Input: nums = [3,7]
+Output: 12
+
+
+Constraints:
+
+2 <= nums.length <= 500
+1 <= nums[i] <= 10^3
+*/
+
+/*
+Solution 1:
+iterate array to find largest 2 number
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func maxProduct(_ nums: [Int]) -> Int {
+        var num1 = 0
+        var num2 = 0
+        for num in nums {
+            if num > num1 {
+                num2 = num1
+                num1 = num
+            } else if num > num2 {
+                num2 = num
+            }
+        }
+        return (num1-1)*(num2-1)
+    }
+}
