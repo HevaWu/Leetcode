cbf93b51eba19199366902cff903b0771726f13e
He Wu <hewu9375@gmail.com>
2024-08-01 18:37:01 +0900

    add	new file:   Number_of_Senior_Citizens.swift

---
+++ Number_of_Senior_Citizens.swift
@@ -0,0 +1,58 @@
+/*
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+The first ten characters consist of the phone number of passengers.
+The next character denotes the gender of the person.
+The following two characters are used to indicate the age of the person.
+The last two characters determine the seat allotted to that person.
+Return the number of passengers who are strictly more than 60 years old.
+
+
+
+Example 1:
+
+Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+Output: 2
+Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+Example 2:
+
+Input: details = ["1313579440F2036","2921522980M5644"]
+Output: 0
+Explanation: None of the passengers are older than 60.
+
+
+Constraints:
+
+1 <= details.length <= 100
+details[i].length == 15
+details[i] consists of digits from '0' to '9'.
+details[i][10] is either 'M' or 'F' or 'O'.
+The phone numbers and seat numbers of the passengers are distinct.
+*/
+
+/*
+Solution 1:
+iterate each person detail, and check each person age
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func countSeniors(_ details: [String]) -> Int {
+        var num = 0
+        for d in details {
+            let age = ageOfPerson(d)
+            if age > 60 {
+                num += 1
+            }
+        }
+        return num
+    }
+
+    func ageOfPerson(_ person: String) -> Int {
+        var person = Array(person)
+        var age = person[11].wholeNumberValue!
+        age = age * 10 + person[12].wholeNumberValue!
+        return age
+    }
+}
