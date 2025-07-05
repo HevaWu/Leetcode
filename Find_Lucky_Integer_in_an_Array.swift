19ed1776db227cf91051efd413ed88276e6b63b6
He Wu <hewu9375@gmail.com>
2025-07-05 20:52:33 +0900

    add Find_Lucky_Integer_in_an_Array.swift

---
+++ Find_Lucky_Integer_in_an_Array.swift
@@ -0,0 +1,54 @@
+/*
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+
+
+Example 1:
+
+Input: arr = [2,2,3,4]
+Output: 2
+Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+Example 2:
+
+Input: arr = [1,2,2,3,3,3]
+Output: 3
+Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+Example 3:
+
+Input: arr = [2,2,2,3,3]
+Output: -1
+Explanation: There are no lucky numbers in the array.
+
+
+Constraints:
+
+1 <= arr.length <= 500
+1 <= arr[i] <= 500
+
+*/
+
+/*
+Solution 1:
+build freq map, then find the largest lucky key in this map
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    func findLucky(_ arr: [Int]) -> Int {
+        var freq = [Int: Int]()
+        for num in arr {
+            freq[num, default: 0] += 1
+        }
+
+        var res = -1
+        for (k,v) in freq {
+            if k == v {
+                res = max(res, k)
+            }
+        }
+        return res
+    }
+}
