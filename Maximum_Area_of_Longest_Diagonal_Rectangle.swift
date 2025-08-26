7f6431e47e7c13f263510d8efaf0b1f4c4e8e6f9
He Wu <hewu9375@gmail.com>
2025-08-26 21:04:02 +0900

    add Maximum_Area_of_Longest_Diagonal_Rectangle.swift

---
+++ Maximum_Area_of_Longest_Diagonal_Rectangle.swift
@@ -0,0 +1,56 @@
+/*
+You are given a 2D 0-indexed integer array dimensions.
+
+For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the length and dimensions[i][1] represents the width of the rectangle i.
+
+Return the area of the rectangle having the longest diagonal. If there are multiple rectangles with the longest diagonal, return the area of the rectangle having the maximum area.
+
+
+
+Example 1:
+
+Input: dimensions = [[9,3],[8,6]]
+Output: 48
+Explanation:
+For index = 0, length = 9 and width = 3. Diagonal length = sqrt(9 * 9 + 3 * 3) = sqrt(90) ≈ 9.487.
+For index = 1, length = 8 and width = 6. Diagonal length = sqrt(8 * 8 + 6 * 6) = sqrt(100) = 10.
+So, the rectangle at index 1 has a greater diagonal length therefore we return area = 8 * 6 = 48.
+Example 2:
+
+Input: dimensions = [[3,4],[4,3]]
+Output: 12
+Explanation: Length of diagonal is the same for both which is 5, so maximum area = 12.
+
+
+Constraints:
+
+1 <= dimensions.length <= 100
+dimensions[i].length == 2
+1 <= dimensions[i][0], dimensions[i][1] <= 100
+*/
+
+/*
+Solution 1:
+record maxArea and maxDiagonal
+need to split logic when finding larger and the equal diagonal dimension
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    func areaOfMaxDiagonal(_ dimensions: [[Int]]) -> Int {
+        var maxArea = 0
+        var maxDiagonal = 0
+        for d in dimensions {
+            let diagonal = d[0] * d[0] + d[1] * d[1]
+            if diagonal > maxDiagonal {
+                // print(d[0], d[1], diagonal, maxDiagonal)
+                maxArea =  d[0] * d[1]
+                maxDiagonal = diagonal
+            } else if diagonal == maxDiagonal {
+                maxArea = max(maxArea, d[0] * d[1])
+            }
+        }
+        return maxArea
+    }
+}
